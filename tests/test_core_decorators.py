812c489776576f5a5591b5851f03f96ab7353651
Peter Volf <do.volfp@gmail.com>
2024-08-27 17:11:11 +0200

    added error rendering to core decorators + preserve response status code and background task, refs #26 #27 #28

--- tests/test_core_decorators.py
+++ tests/test_core_decorators.py
@@ -21,8 +21,22 @@ async def async_render_user_list(result: list[User], *, context: dict[str, Any],
     return render_user_list(result, context=context, request=request)
 
 
+class DataError(Exception):
+    def __init__(self, message: str, response: Response) -> None:
+        self.message = message
+        # Highlight how to set the response code for a route that has error rendering.
+        response.status_code = 499
+
+
+def render_data_error(result: Exception, *, context: dict[str, Any], request: Request) -> str:
+    if isinstance(result, DataError):
+        return f'<DataError message="{result.message}" />'
+
+    raise result
+
+
 @pytest.fixture
-def hx_app() -> FastAPI:
+def hx_app() -> FastAPI:  # noqa: C901
     app = FastAPI()
 
     @app.get("/")
@@ -36,9 +50,42 @@ def hx_app() -> FastAPI:
         response.headers["test-header"] = "exists"
         return users
 
-    @app.get("/htmx-only")  # type: ignore # TODO: figure out why mypy doesn't see the correct type.
+    # There's a strange mypy issue here, it finds errors for the routes that defined later,
+    # regardless of the order. It seems it fails to resolve and match generic types.
+
+    @app.get("/htmx-only")  # type: ignore
     @hx(async_render_user_list, no_data=True)
     async def htmx_only(random_number: DependsRandomNumber) -> list[User]:
+        return users
+
+    @app.get("/error/{kind}")  # type: ignore
+    @hx(render_user_list, render_error=render_data_error)
+    def error_in_route(kind: str, response: Response) -> list[User]:
+        if kind == "data":
+            raise DataError("test-message", response)
+        elif kind == "value":
+            raise ValueError("Value error was requested.")
+
+        return users
+
+    @app.get("/error-no-data/{kind}")  # type: ignore
+    @hx(render_user_list, render_error=render_data_error, no_data=True)
+    def error_in_route_no_data(kind: str, response: Response) -> list[User]:
+        if kind == "data":
+            raise DataError("test-message", response)
+        elif kind == "value":
+            raise ValueError("Value error was requested.")
+
+        return users
+
+    @app.get("/error-page/{kind}")  # type: ignore
+    @page(render_user_list, render_error=render_data_error)
+    def error_in_route_page(kind: str, response: Response) -> list[User]:
+        if kind == "data":
+            raise DataError("test-message", response)
+        elif kind == "value":
+            raise ValueError("Value error was requested.")
+
         return users
 
     return app
@@ -46,7 +93,7 @@ def hx_app() -> FastAPI:
 
 @pytest.fixture
 def hx_client(hx_app: FastAPI) -> TestClient:
-    return TestClient(hx_app)
+    return TestClient(hx_app, raise_server_exceptions=False)
 
 
 @pytest.mark.parametrize(
@@ -83,3 +130,30 @@ def test_hx_and_page(
     assert result == expected
 
     assert all((response.headers.get(key) == value) for key, value in response_headers.items())
+
+
+@pytest.mark.parametrize(
+    ("route", "headers", "status", "expected"),
+    (
+        ("/error/data", {"HX-Request": "true"}, 499, '<DataError message="test-message" />'),
+        ("/error/data", None, 500, None),  # No rendering, internal server error
+        ("/error/value", {"HX-Request": "true"}, 500, None),  # No rendering for value route
+        ("/error-no-data/data", {"HX-Request": "true"}, 499, '<DataError message="test-message" />'),
+        ("/error-no-data/data", None, 400, None),  # No data, bad request
+        ("/error-no-data/value", {"HX-Request": "true"}, 500, None),  # No rendering for value route
+        ("/error-page/data", {"HX-Request": "true"}, 499, '<DataError message="test-message" />'),
+        ("/error-page/data", None, 499, '<DataError message="test-message" />'),  # Rendering non-HX request
+        ("/error-page/value", {"HX-Request": "true"}, 500, None),  # No rendering for value route
+    ),
+)
+def test_hx_and_page_error_rendering(
+    hx_client: TestClient,
+    route: str,
+    headers: dict[str, str] | None,
+    status: int,
+    expected: str | None,
+) -> None:
+    response = hx_client.get(route, headers=headers)
+    assert response.status_code == status
+    if expected is not None:
+        assert response.text == expected
