87bafdf0a13220ba1e8ce0ca8324ec31119a114e
Peter Volf <do.volfp@gmail.com>
2025-02-23 13:20:43 +0100

    feat: better FastAPI Request aliasing in dependencies.py, fixes #53

--- fasthx/dependencies.py
+++ fasthx/dependencies.py
@@ -1,22 +1,26 @@
 from collections.abc import Mapping
 from typing import TYPE_CHECKING, Annotated, Any, TypeAlias
 
-from fastapi import Depends, Header, Request
+from fastapi import Depends, Header
+from fastapi import Request as FARequest
 
-RequestAlias: TypeAlias = Mapping[str, Any]
-"""
-Alias for `Request` arguments.
+if TYPE_CHECKING:
+    Request: TypeAlias = FARequest
+else:
+    Request: TypeAlias = Mapping[str, Any]
+    """
+    Alias for `Request` arguments.
 
-Workaround for this FastAPI bug: https://github.com/fastapi/fastapi/discussions/12403.
-And here's a FastAPI bugfix: https://github.com/fastapi/fastapi/pull/12406.
+    Workaround for this FastAPI bug: https://github.com/fastapi/fastapi/discussions/12403.
+    And here's a FastAPI bugfix: https://github.com/fastapi/fastapi/pull/12406.
 
-This workaround should be removed when FastAPI had several new releases with the fix.
-"""
+    This workaround should be removed when FastAPI had several new releases with the fix.
+    """
 
 
 def get_hx_request(
-    request: Request, hx_request: Annotated[str | None, Header()] = None
-) -> RequestAlias | None:
+    request: FARequest, hx_request: Annotated[str | None, Header()] = None
+) -> Request | None:
     """
     FastAPI dependency that returns the current request if it is an HTMX one,
     i.e. it contains an `"HX-Request: true"` header.
@@ -24,7 +28,7 @@ def get_hx_request(
     return request if hx_request == "true" else None
 
 
-def get_page_request(request: Request) -> RequestAlias:
+def get_page_request(request: FARequest) -> Request:
     """
     Replacement dependency for `Request` to work around this FastAPI bug:
     https://github.com/fastapi/fastapi/discussions/12403.
@@ -32,16 +36,12 @@ def get_page_request(request: Request) -> RequestAlias:
     return request
 
 
-if TYPE_CHECKING:
-    DependsHXRequest: TypeAlias = Request | None
-    DependsPageRequest: TypeAlias = Request
-else:
-    DependsHXRequest = Annotated[RequestAlias | Request | None, Depends(get_hx_request)]
-    """Annotated type (dependency) for `get_hx_request()` for FastAPI."""
+DependsHXRequest = Annotated[Request | None, Depends(get_hx_request)]
+"""Annotated type (dependency) for `get_hx_request()` for FastAPI."""
 
-    DependsPageRequest = Annotated[RequestAlias | Request, Depends(get_page_request)]
-    """
-    Annotated `Request` dependency alias.
+DependsPageRequest = Annotated[Request, Depends(get_page_request)]
+"""
+Annotated `Request` dependency alias.
 
-    Workaround for this FastAPI bug: https://github.com/fastapi/fastapi/discussions/12403
-    """
+Workaround for this FastAPI bug: https://github.com/fastapi/fastapi/discussions/12403
+"""
